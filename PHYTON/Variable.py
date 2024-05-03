2c2b53188b10042df329d432ff10dd521723a29f
CODEX1235 <147001058+CODEX1235@users.noreply.github.com>
2024-05-03 17:58:02 -0500

    Add files via upload

---
+++ PHYTON/Variable.py
@@ -0,0 +1,78 @@
+#name = "Pepito"
+
+#Last_name="Perez"
+
+#email = "Pepito@gmail.com"
+
+#salary = 1300000
+
+#state = True
+
+#note = 4,5
+
+#gender = "M"
+
+#print(email)
+
+#print("Nombre:"+ name)
+#concatenar usando ,
+#print("correo",email)
+
+#concatenar de contencion formato 
+#print( f"Nombre {name}\n Apellido{Last_name} \n Coreo{email} \n salario{salary}")
+
+#print(type(salary))
+
+#Como capturar datos desde la consola input()
+
+#phone = input("Telefono: ") 
+
+#print(f"Telefono:{phone}")
+
+#aux_transporte = int(input("Ingrese el aux de transporte"))
+
+#total_salary = salary+aux_transporte
+
+#print(f"Su salario total es {total_salary}")
+
+#per_loan_discount = 0.3
+
+#loan_discount = salary + per_loan_discount
+
+#total_salary = salary + aux_transporte + loan_discount
+
+#print(f"El salario total es {total_salary}\n Descuentos: \nprestamos{loan_discount} ")
+
+id= "SKU98453"
+
+print(f"id : {id}")
+
+nombre = "YOGURT ORIGINAL ALPINA FRESA"
+
+print(f"nombre : {nombre}")
+
+Descripcion = "El yogur es producto de leche coagulada obtenida por la fermentación láctica producida por la acción de las bacterias Lactobacillus bulgaricus y Streptococcus thermophilus."
+
+print(f" Descripcion del producto: {Descripcion}")
+
+Costo = 2500
+
+cantidad= 1
+
+print(f" cantidad del producto: {cantidad}")
+
+Estado= "Comestible"
+
+print(f"Estado del producto:"+Estado )
+
+Precio = Costo/( 1 - 30/100)
+
+print(f"El producto {nombre}  tiene un margen de ganancia de:{Precio}")
+
+
+
+
+
+
+
+
